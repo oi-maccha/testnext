ccc1e67368cce6439bc42de366809803a7df33cf
oimattya <86579929+018matsumoto@users.noreply.github.com>
2022-12-07 17:10:50 +0900

    SSG設定調整

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/testnext" : "",
+  trailingSlash: true,
+  images: {
+    // 画像最適化：OFF（利用先を設定しないとVercel以外SSG不可）
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
